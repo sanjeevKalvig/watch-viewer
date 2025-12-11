a0bddfa510507abde58b7a2f8ea56059f3710165
sanjeevKalvig <sanjeev.kumar@kalvig.in>
2025-12-11 09:56:07 +0530

    size change

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,7 @@ export default function App() {
       </div>
 
       <Canvas
-        camera={{ position: [0, 0, 1.2], fov: 60 }}
+        camera={{ position: [0, 0, 1.2], fov: 65 }}
         dpr={[2, 3]}
         gl={{
           antialias: true,
